b20acde5780bc3342e6a88a5f3d2e809eb0262e7
Niyaz <khairutdinov96@gmail.com>
2023-01-24 22:58:51 +0300

    a little refactoring

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,21 @@
 function getCoupeNumber(placeNum) {
     if (isNaN(placeNum) === true || placeNum % 1 !== 0 ||  placeNum < 0 ) {
-        return "Ошибка. Проверьте правильность введенного числа.";
+        return "Ошибка. Проверьте правильность введенного номера места.";
     } else if (placeNum === 0 || placeNum > 36) {
-        return "Таких мест в вагоне не существует";
+        return "Таких мест в вагоне не существует.";
     } else if ((placeNum / 2) % 2 === 0) {
         const evenResult = (placeNum / 2) / 2;
-        return evenResult;
+        return `Номер вашего купе - ${evenResult}.`;
     } else if ((placeNum / 2) % 2 === 1) {
         const evenResult = (placeNum / 2) / 2 + 0.5;
-        return evenResult;
+        return `Номер вашего купе - ${evenResult}.`;
     } else if ((placeNum / 2) % 2 === 0.5){
         const evenResult = (placeNum / 2) / 2 + 0.75;
-        return evenResult;
+        return `Номер вашего купе - ${evenResult}.`;
     } else if ((placeNum / 2) % 2 === 1.5){
         const evenResult = (placeNum / 2) / 2 + 0.25;
-        return evenResult;
+        return `Номер вашего купе - ${evenResult}.`;
     } 
 } 
 
-console.log(getCoupeNumber(37));
-
+console.log(getCoupeNumber(33));
