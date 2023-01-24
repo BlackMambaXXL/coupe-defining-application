5bb4a2a73726c91b7eeef561d8b6e9484682f055
Niyaz <khairutdinov96@gmail.com>
2023-01-24 16:51:57 +0300

    testing odd num

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,9 @@ function getCoupeNumber(placeNum) {
     } else if ((placeNum / 2) % 2 === 1) {
         const evenResult = (placeNum / 2) / 2 + 0.5;
         console.log(`Номер вашего купе - ${evenResult}`);
-    }  
+    } else if ((placeNum / 2) % 2 === 0.5){
+        const evenResult = (placeNum / 2) / 2 + 0.75;
+        console.log(`Номер вашего купе - ${evenResult}`);
+    } 
 }
 getCoupeNumber(34);
