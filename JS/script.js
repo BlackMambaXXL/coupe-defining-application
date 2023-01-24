1117b1d40cc4793f7d29c785eb0b76e9845e7a5e
Niyaz <khairutdinov96@gmail.com>
2023-01-24 17:22:17 +0300

    testing function validation

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,19 @@
 function getCoupeNumber(placeNum) {
-    if ((placeNum / 2) % 2 === 0) {
+    if (isNaN(placeNum) === true || placeNum % 2 != (0 || 1) || placeNum < 0 ) {
+        console.log("Ошибка. Проверьте правильность введенного числа.");
+    } else if ((placeNum / 2) % 2 === 0) {
         const evenResult = (placeNum / 2) / 2;
-        console.log(`Номер вашего купе - ${evenResult}`);
+        console.log(`Номер вашего купе - ${evenResult}.`);
     } else if ((placeNum / 2) % 2 === 1) {
         const evenResult = (placeNum / 2) / 2 + 0.5;
-        console.log(`Номер вашего купе - ${evenResult}`);
+        console.log(`Номер вашего купе - ${evenResult}.`);
     } else if ((placeNum / 2) % 2 === 0.5){
         const evenResult = (placeNum / 2) / 2 + 0.75;
-        console.log(`Номер вашего купе - ${evenResult}`);
+        console.log(`Номер вашего купе - ${evenResult}.`);
     } else if ((placeNum / 2) % 2 === 1.5){
         const evenResult = (placeNum / 2) / 2 + 0.25;
-        console.log(`Номер вашего купе - ${evenResult}`);
-    } 
+        console.log(`Номер вашего купе - ${evenResult}.`);
+    }
 }
-getCoupeNumber(34);
+getCoupeNumber(-16);
+
