e10f665d73ccb0120f72ed739cd320489739a1b2
Niyaz <khairutdinov96@gmail.com>
2023-01-24 16:51:34 +0300

    testing second even num

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,9 @@ function getCoupeNumber(placeNum) {
     if ((placeNum / 2) % 2 === 0) {
         const evenResult = (placeNum / 2) / 2;
         console.log(`Номер вашего купе - ${evenResult}`);
+    } else if ((placeNum / 2) % 2 === 1) {
+        const evenResult = (placeNum / 2) / 2 + 0.5;
+        console.log(`Номер вашего купе - ${evenResult}`);
     }  
 }
 getCoupeNumber(34);
