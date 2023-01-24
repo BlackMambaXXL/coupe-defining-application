c63610eaf3f6f3adfad8b2bfea3e27c880a70be7
Niyaz <khairutdinov96@gmail.com>
2023-01-24 16:52:18 +0300

    testing second odd num

--- JS/script.js
+++ JS/script.js
@@ -8,6 +8,9 @@ function getCoupeNumber(placeNum) {
     } else if ((placeNum / 2) % 2 === 0.5){
         const evenResult = (placeNum / 2) / 2 + 0.75;
         console.log(`Номер вашего купе - ${evenResult}`);
+    } else if ((placeNum / 2) % 2 === 1.5){
+        const evenResult = (placeNum / 2) / 2 + 0.25;
+        console.log(`Номер вашего купе - ${evenResult}`);
     } 
 }
 getCoupeNumber(34);
